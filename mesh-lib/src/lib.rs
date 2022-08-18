00a2aafdfa4e2d514fe35b34a7637c185e19187e
bistin <bistin@fugle.tw>
2022-08-18 23:54:57 +0800

    can display cp

---
+++ mesh-lib/src/lib.rs
@@ -0,0 +1,30 @@
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize,Debug)]
+pub struct Fold {
+    pub file_spec: f32,
+    pub file_creator: String,
+    pub file_author: String,
+    pub file_classes: Vec<String>,
+    pub frame_attributes: Vec<String>,
+    pub frame_unit: String,
+    pub vertices_coords: Vec<[f32;3]>,
+    pub edges_vertices:  Vec<[i32;2]>,
+    pub edges_assignment: Vec<String>,
+    pub faces_vertices: Vec<[i32;3]>,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use super::*;
+
+    #[test]
+    fn test_parser() {
+        let data = fs::read_to_string("./src/crand.fold").unwrap();
+        let p: Fold = serde_json::from_str(&data).unwrap();
+        println!("{:?}", p);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use bevy::prelude::*;
 use bevy::render::mesh::{self, PrimitiveTopology};
 use bevy_inspector_egui::WorldInspectorPlugin;
+use mesh_lib::Fold;
+use std::fs;
+
+use bevy::{
+    asset::{AddAsset, AssetLoader, LoadContext, LoadedAsset},
+    prelude::*,
+    render::{
+        mesh::{Indices, Mesh, VertexAttributeValues},
+    },
+    utils::BoxedFuture,
+};
+
 
 fn main() {
     App::new()
@@ -11,49 +23,84 @@ fn main() {
         .run();
 }
 
+
+// fn stl_to_wireframe_mesh(stl: &stl_io::IndexedMesh) -> Mesh {
+//     let mut mesh = Mesh::new(PrimitiveTopology::LineList);
+
+//     let positions = stl.vertices.iter().map(|v| [v[0], v[1], v[2]]).collect();
+//     let mut indices = Vec::with_capacity(stl.faces.len() * 3);
+//     let normals = vec![[1.0, 0.0, 0.0]; stl.vertices.len()];
+//     let uvs = vec![[0.0, 0.0]; stl.vertices.len()];
+
+//     for face in &stl.faces {
+//         for j in 0..3 {
+//             indices.push(face.vertices[j] as u32);
+//             indices.push(face.vertices[(j + 1) % 3] as u32);
+//         }
+//     }
+//     print!("{:?}", indices);
+//     mesh.insert_attribute(
+//         Mesh::ATTRIBUTE_POSITION,
+//         VertexAttributeValues::Float32x3(positions),
+//     );
+//     mesh.insert_attribute(
+//         Mesh::ATTRIBUTE_NORMAL,
+//         VertexAttributeValues::Float32x3(normals),
+//     );
+//     mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, VertexAttributeValues::Float32x2(uvs));
+//     mesh.set_indices(Some(Indices::U32(indices)));
+
+//     mesh
+// }
+
 /// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     ) {
-    let vertices = [
-        ([-1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
-        ([1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
-        ([0.0, 3.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0]),
-        ([0.0, -3.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0])
-    ];
+   
 
-    let indices = mesh::Indices::U32(vec![0, 2, 1, 3, 1, 2,0,3,2]);
+    let data = fs::read_to_string("./mesh-lib/src/crand.fold").unwrap();
+    let fold: Fold = serde_json::from_str(&data).unwrap();
 
-    let mut positions = Vec::new();
-    let mut normals = Vec::new();
-    let mut uvs = Vec::new();
-    for (position, normal, uv) in vertices.iter() {
-        positions.push(*position);
-        normals.push(*normal);
-        uvs.push(*uv);
+    println!("{:?}", fold);
+
+    let positions = fold.vertices_coords;
+    let mut indices = Vec::with_capacity(fold.faces_vertices.len() * 3);
+    let normals = vec![[0.0, 1.0, 0.0]; positions.len()];
+    let uvs = vec![[0.0, 0.0]; positions.len()];
+
+    for face in &fold.faces_vertices {
+        for j in 0..3 {
+            indices.push(face[j] as u32);
+            indices.push(face[(j + 1) % 3] as u32);
+        }
     }
+    print!("{:?}", indices);
 
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.set_indices(Some(indices));
+
+
+
+    let mut mesh = Mesh::new(PrimitiveTopology::LineList);
+    mesh.set_indices(Some(Indices::U32(indices)));
     mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
     mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
     //mesh.insert_attribute(Mesh::ATTRIBUTE_JOINT_WEIGHT, vec![1.0,1.0,1.0,1.0]);
     //mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
-
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, VertexAttributeValues::Float32x2(uvs));
     // add entities to the world
 
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
+    // commands.spawn_bundle(PbrBundle {
+    //     mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+    //     material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    //     ..default()
+    // });
 
     // plane
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(mesh),
-        //material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
         ..default()
     });
     // light
